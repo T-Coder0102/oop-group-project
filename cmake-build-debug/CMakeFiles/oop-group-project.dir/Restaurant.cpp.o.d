fa2f0e2826bc1b60d56e2aee83e53b2653bc57a2
T-Coder0102 <t.joldasbayev@newuu.uz>
2025-03-20 15:36:05 +0500

    oop1-2003/2025

---
+++ cmake-build-debug/CMakeFiles/oop-group-project.dir/Restaurant.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/oop-group-project.dir/Restaurant.cpp.o: \
+  /Users/t.joldasbayev/CLionProjects/oop-group-project/Restaurant.cpp \
+  /Users/t.joldasbayev/CLionProjects/oop-group-project/Restaurant.h
