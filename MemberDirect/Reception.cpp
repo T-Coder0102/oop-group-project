846e31faefd988d080a479c1294f045a8faba4df
Timur <timurjoldasbayev03@gmail.com>
2025-04-11 22:58:53 +0500

    oop-1 1104/2025-3

--- MemberDirect/Reception.cpp
+++ MemberDirect/Reception.cpp
@@ -19,7 +19,8 @@ Client * Reception::askInfo(Hotel &hotel) {
     cin>>userName;
     cout<<"Enter your password:"<<endl;
     cin>>userPassword;
-    Client *c = new Client(name,age,gender,1000+rand()%1000,userName,userPassword);
+    int id=1000+rand()%1000;
+    Client *c = new Client(name,age,gender,id,userName,userPassword);
     hotel.addClientsToList(c);
     cout<<"Thank you for your information"<<endl;
     return c;
